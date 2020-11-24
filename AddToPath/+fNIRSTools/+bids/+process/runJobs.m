a53c4a39fcb79b68ddfee249cb35e98b282f46e5
kmstubbs <kmstubbs@gmail.com>
2020-11-24 09:45:18 -0500

    add bids module
    examples and more documentation to come

---
+++ AddToPath/+fNIRSTools/+bids/+process/runJobs.m
@@ -0,0 +1,144 @@
+% analyzir_bids_process_runJobs(bids_info, source_type, target_type, jobs, overwrite)
+%
+% Runs a job set on individual runs. Existing outputs will be skipped by
+% default.
+%
+% Inputs:
+%   bids_info       bids_info structure specifying datasets to use
+%   source_type     naming scheme of input file (e.g., snirf)
+%   target_type     naming scheme of output file (e.g., hb)
+%   jobs            jobs to be performed on each run
+%   overwrite       if true, redo and overwrite existing
+%   flags           optional cell array of strings
+%                       decon = save subset of decon output (<1mb compared to >600mb)
+%                       group = combine runs and perform jobs on the set
+function analyzir_bids_process_runJobs(bids_info, input_type, output_type, jobs, overwrite, flags)
+
+%% Defaults
+
+if ~exist('overwrite', 'var')
+    overwrite = false;
+end
+
+if ~exist('flags', 'var')
+    flags = cell(0);
+else
+    if ~iscell(flags)
+        flags = {flags};
+    end
+    flags = cellfun(@lower, flags, 'UniformOutput', false);
+end
+
+
+%% Upper Case Types
+
+input_type = upper(input_type);
+output_type = upper(output_type);
+
+
+%% Handle BIDS Info
+
+[filepaths_input,exists_input] = analyzir_bids.io.getFilepath(input_type, bids_info, true);
+[filepaths_output,exists_output] = analyzir_bids.io.getFilepath(output_type, bids_info, true);
+
+
+%% Get Job Info To Display
+
+fprintf('Performing [%s => %s]...\n', input_type, output_type);
+
+job_list = [];
+job = jobs;
+while ~isempty(job)
+    job_list{end+1} = job;
+    job = job.prevJob;
+end
+
+for job = job_list(end:-1:1)
+    disp(job{1})
+end
+
+
+%% Method 1: Indiv
+
+tic
+if ~any(strcmp(flags, 'group'))
+    for i = 1:bids_info.number_datasets
+        fprintf('\tSet %03d of %03d (%s)\n', i, bids_info.number_datasets, bids_info.datasets(i).full_name);
+
+        if exists_output(i) && ~overwrite
+            fprintf('\t\tOutput already exists and overwrite is false, skipping!\n');
+        else
+            if ~exists_input(i)
+                error('Missing input file: %s', exists_input(i));
+            else
+                %load
+                fprintf('\t\tLoading %s: %s\n', input_type, filepaths_input{i});
+                input = analyzir_bids.io.readFile(bids_info, input_type, i);
+
+                %process
+                runSingleSet(input, jobs, output_type, filepaths_output{i}, flags, sprintf('\t\t'));
+            end
+        end
+        
+        %done
+        fprintf('\t\tFinished at %g seconds\n', toc);
+    end
+
+%% Method 2: Group
+else
+    %output
+    filepath_output = [bids_info(1).root_directory 'derivatives' filesep sprintf('Group_%s.mat', output_type)];
+    if exist(filepath_output, 'file') && ~overwrite
+        error('Output already exists and overwrite is false, skipping: %s', filepath_output)
+    else
+    
+        %load all
+        fprintf('Loading all input %s...\n', input_type);
+        inputs = analyzir_bids.io.readFile(bids_info, input_type);
+        
+        %process
+        runSingleSet(inputs, jobs, output_type, filepath_output, flags, '');
+
+        %done
+        fprintf('Finished at %g seconds\n', toc);
+
+    end
+end
+
+
+
+
+function runSingleSet(input, jobs, output_type, filepath_output, flags, print_prefix)
+
+%perform
+fprintf('%sRunning jobs...\n', print_prefix);
+if strcmp(output_type, 'RAW') && isempty(jobs)
+    %no jobs when converting raw
+    data = input;
+else
+    text = evalc('data = jobs.run(input);'); %redirect messages
+end
+
+%if decon, save only the beta x time x cond x channel (else
+%very large files)
+if any(strcmp(flags, 'decon'))
+    fprintf('%sReducing data for decon save...\n', print_prefix);
+    try
+        data = data.HRF;
+    catch
+        warning('Unable to reduce decon data (might not be decon output)');
+    end
+end
+
+%save
+fprintf('%sSaving %s: %s\n', print_prefix, output_type, filepath_output);
+try
+    save(filepath_output, 'data');
+catch
+    warning('Standard save failed. Using v7.3 method, which supports larger files but takes longer to load.')
+    save(filepath_output, 'data', '-v7.3');
+end
+            
+            
+
+
---
+++ AddToPath/+fNIRSTools/+bids/+util/checkMontages.m
@@ -0,0 +1,121 @@
+% Reads all snirf files and confirms that they used the same montage
+% If "complete" is true, then all comparisons are made.
+function [all_identical] = analyzir_bids_util_checkMontages(bids_info, complete)
+
+%% Inputs
+
+if ~exist('complete', 'var')
+    complete = false;
+end
+
+%% Read Montages
+
+%if all runs have raw or HB mat, it's much faster to get montages from there
+[~,exists_raw] = fNIRSTools.bids.io.getFilepath('RAW', bids_info, true);
+if ~any(~exists_raw)
+	files = fNIRSTools.bids.io.readFile(bids_info, 'RAW');
+else
+    warning('Did not locate full set of raw mat files. Reading directly from SNIRF instead, which is slower.')
+    files = fNIRSTools.bids.io.readFile(bids_info, 'SNIRF');
+end
+
+montages = arrayfun(@(f) f.probe, files);
+number_montages = length(montages);
+if number_montages < 2
+    error('Found less than 2 montages')
+end
+
+%% Compare
+
+if ~complete
+    fprintf('Comparing montages (reduced comparison method)...\n');
+    
+    montage_ref = montages(1);
+    same = [1; arrayfun(@(m) compareMontage(montage_ref, m), montages(2:end))];
+    
+    montage_matches_first = [{bids_info.datasets.full_name}' num2cell(same)];
+    disp 'Results of comparison to first montage (1=same, 0=diff):'
+    disp(montage_matches_first)
+else
+    fprintf('Comparing montages (complete comparison method)...\n');
+    
+    %make all comparisons
+    same = nan(number_montages,number_montages);
+    for m1 = 1:number_montages
+        for m2 = m1:number_montages
+            same(m1,m2) = compareMontage(montages(m1),montages(m2));
+            same(m2,m1) = same(m1,m2);
+        end
+    end
+    
+    %figure
+    fig = figure('Position', get(0,'ScreenSize'));
+    set(gcf,'color','k');
+    set(gca, 'Color', 'k', 'GridColor', 'w', 'MinorGridColor', 'w', 'XColor', 'w', 'YColor', 'w');
+    imagesc(same)
+    axis square
+    cb = colorbar;
+    set(cb, 'ytick', [0.05 0.95], 'yticklabel', {'Different' 'Same'});
+    colormap([0 0 0.5; 0 0.5 0]);
+    caxis([0 1]);
+    set(gca, 'xtick', 0.5:1:number_montages, 'xticklabel', 1:number_montages, 'ytick', 1:number_montages, 'yticklabel', cellfun(@(x) strrep(x, '_', ''), {bids_info.datasets.full_name}, 'UniformOutput', false))
+    grid on
+    title('Montage Comparison', 'Color', 'white')
+    
+    filepath = fNIRSTools.bids.io.getFilepath('MONTAGE_COMPARISON_FIG', bids_info, false, true);
+    fprintf('Writing: %s\n', filepath);
+    img = getframe(gcf);
+    imwrite(img.cdata,filepath);
+    close(fig);
+end
+
+all_identical = ~any(~same(:));
+if ~all_identical
+    warning('Montages are not all identical')
+end
+
+
+
+function [same] = compareMontage(montage_source, montage_target)
+
+%default
+same = true;
+
+%link table
+if any(size(montage_source.link) ~= size(montage_target.link))
+    same = false;
+    return
+else
+    check = (montage_source.link.detector ~= montage_target.link.detector) | ...
+            (montage_source.link.source ~= montage_target.link.source) | ...
+            (montage_source.link.type ~= montage_target.link.type);
+    if any(check(:))
+        same = false;
+        return
+    end
+end
+
+%source positions
+if any(size(montage_source.srcPos) ~= size(montage_target.srcPos))
+    same = false;
+    return
+else
+    check = montage_source.srcPos ~= montage_target.srcPos;
+    if any(check(:))
+        same = false;
+        return
+    end
+end
+
+%detector positions
+if any(size(montage_source.detPos) ~= size(montage_target.detPos))
+    same = false;
+    return
+else
+    check = montage_source.detPos ~= montage_target.detPos;
+    if any(check(:))
+        same = false;
+        return
+    end
+end
+
