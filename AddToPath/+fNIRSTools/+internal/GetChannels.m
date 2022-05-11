e91975d4b6026ce1549d0c158124dc7e4a13bf8e
kmstubbs <kmstubbs@gmail.com>
2022-05-11 17:06:26 -0400

    Create GetChannels.m

---
+++ AddToPath/+fNIRSTools/+internal/GetChannels.m
@@ -0,0 +1,45 @@
+% [channels,has_sdc_info] = GetChannels(data)
+%
+% Returns "channels" table with:
+%   Source
+%   Detector
+%   TypeIndices
+%   ShortSeperation (if provided)
+%
+function [channels,has_sdc_info] = GetChannels(data)
+
+%unique s-d pairs
+channels = unique(data.probe.link(:,1:2), 'rows');
+number_channels = height(channels);
+
+%find channel-type pairs
+types = data.probe.types;
+number_types = length(types);
+inds = cell(number_channels,1);
+for c = 1:number_channels
+    for t = 1:number_types
+        
+        ind = data.probe.link.source==channels.source(c) & ...
+                    data.probe.link.detector==channels.detector(c);
+                
+        if isnumeric(types)
+            ind = ind & (data.probe.link.type == types(t));
+        else
+            ind = ind & strcmp(data.probe.link.type, types{t});
+        end
+        
+        ind = find(ind);
+        if length(ind) ~= 1
+            error
+        else
+            inds{c}(t) = ind;
+        end
+    end
+end
+channels.TypeIndices = inds;
+
+%
+has_sdc_info = any(strcmp(data.probe.link.Properties.VariableNames, 'ShortSeperation'));
+if has_sdc_info
+    channels.ShortSeperation = cellfun(@(i) data.probe.link.ShortSeperation(i(1)), channels.TypeIndices);
+end
